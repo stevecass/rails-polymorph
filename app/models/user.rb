a6828fc0da1485f3200c063e7e1b1a490655655b
Steven Cassidy <steven@mumsnet.com>
2014-10-09 21:27:22 -0400

    Add associations to commented items

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,13 @@
 class User < ActiveRecord::Base
+  has_secure_password
   has_many :photos
   has_many :messages
   has_many :comments
+
+  has_many :commented_messages, through: :comments, 
+    source: :commentable, source_type: 'Message'
+
+  has_many :commented_photos, through: :comments, 
+    source: :commentable, source_type: 'Photo'
+
 end
